162175eac4f9f5a00cf869ad1ff3156954c5442d
tailnode <tailnode@gmail.com>
2014-05-05 22:15:12 +0800

    use GLMatrixStack and GLGeometryTransform

--- learn3DGraphics/projection/projection/main.cpp
+++ learn3DGraphics/projection/projection/main.cpp
@@ -5,37 +5,71 @@
 #include <windows.h>
 #include <StopWatch.h>
 #include <GLFrustum.h>
+#include <GLMatrixStack.h>
+#include <GLGeometryTransform.h>
 
 GLShaderManager shaderManager;
 GLTriangleBatch torusBatch;
+GLTriangleBatch sphereBatch1;
+GLTriangleBatch sphereBatch2;
+GLBatch floorBatch;
 GLFrustum frustum;
+
+GLMatrixStack modelViewMatrix;
+GLMatrixStack projectionMatrix;
+GLGeometryTransform transformPipeline;
 
 void changeSize(int w, int h)
 {
 	if (h == 0) h = 1;
 	glViewport(0, 0, w, h);
-	frustum.SetPerspective(35, (float)w / h, 1, 1000);
+	frustum.SetPerspective(35, (float)w / h, 1, 100);
+    projectionMatrix.LoadMatrix(frustum.GetProjectionMatrix());
+
+    transformPipeline.SetMatrixStacks(modelViewMatrix, projectionMatrix);
 }
 
 void renderScene()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	M3DMatrix44f translateM, rotateM, modelViewM, modelViewProjectionM;
 
     static CStopWatch timer;
-    float angle = timer.GetElapsedSeconds() * 60;
-
-	m3dTranslationMatrix44(translateM, 0, 0, -1.5);
-    m3dRotationMatrix44(rotateM, m3dDegToRad(angle), 0, 1, 0);
-
-    m3dMatrixMultiply44(modelViewM, translateM, rotateM);
-	m3dMatrixMultiply44(modelViewProjectionM, frustum.GetProjectionMatrix(), modelViewM);
+    float torusAngle = timer.GetElapsedSeconds() * 120;
+    float sphereAngle = timer.GetElapsedSeconds() * 120;
 
 	GLfloat red[] = {1.0f, 0.0f, 0.0f, 1.0f};
-	shaderManager.UseStockShader(GLT_SHADER_FLAT, modelViewProjectionM, red);
+    GLfloat black[] = {0.0f, 0.0f, 0.0f, 1.0f};
+    GLfloat green[] = {0.0f, 1.0f, 0.0f, 1.0f};
 
+    modelViewMatrix.PushMatrix();
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), black);
+    floorBatch.Draw();
+
+    modelViewMatrix.Translate(0, 0, -2.5f);
+    modelViewMatrix.Rotate(torusAngle, 0, 1, 0);
+
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), red);
 	torusBatch.Draw();
 	
+    modelViewMatrix.PopMatrix();
+
+    modelViewMatrix.PushMatrix();
+    modelViewMatrix.Translate(0.5f, 0.0f, -2.5f);
+    modelViewMatrix.Rotate(sphereAngle, 0, -1, 0);
+    modelViewMatrix.Translate(0.5f, 0.0f, 0.0f);
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), green);
+    sphereBatch1.Draw();
+
+    modelViewMatrix.PopMatrix();
+
+    modelViewMatrix.PushMatrix();
+    modelViewMatrix.Translate(-0.5f, 0.0f, -2.5f);
+    modelViewMatrix.Rotate(sphereAngle, 0, 1, 0);
+    modelViewMatrix.Translate(0.5f, 0.0f, 0.0f);
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), green);
+    sphereBatch2.Draw();
+    modelViewMatrix.PopMatrix();
+
 	glutSwapBuffers();
 	glutPostRedisplay();
 }
@@ -48,10 +82,24 @@ void setupRC()
 {
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 	shaderManager.InitializeStockShaders();
-	gltMakeTorus(torusBatch, 0.25f, 0.10f, 30, 30);
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-}
+    glEnable(GL_DEPTH_TEST);
+    
+	gltMakeTorus(torusBatch, 0.25f, 0.10f, 30, 30);
+    gltMakeSphere(sphereBatch1, 0.10f, 30, 15);
+    gltMakeSphere(sphereBatch2, 0.10f, 30, 15);
 
+    floorBatch.Begin(GL_LINES, 324);
+    for (float i = -20; i <= 20; i += 0.5)
+    {
+        floorBatch.Vertex3f(i, -0.5, 20);
+        floorBatch.Vertex3f(i, -0.5, -20);
+        
+        floorBatch.Vertex3f(20, -0.5, i);
+        floorBatch.Vertex3f(-20, -0.5, i);
+    }
+    floorBatch.End();
+}
 int main(int argc, char* argv[])
 {
 	gltSetWorkingDirectory(argv[0]);
